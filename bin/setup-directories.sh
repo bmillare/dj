f7bb2b0e1ccedea19be7edc61fb15aab087a3f8f
Brent Millare <brent.millare@gmail.com>
2010-09-29 13:14:28 -0400

    readme reflects new status

--- bin/setup-directories.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-RUNAS="./bin/setup-directories.sh"
-if [ $0 = $RUNAS ] ; then
-    mkdir -p usr/src usr/bin etc sbin opt tmp/dj var lib
-else
-    echo "Run this only from the project directory"
-    echo "like this:"
-    echo " user:~$ $RUNAS"
-fi
