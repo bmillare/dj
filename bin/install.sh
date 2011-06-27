c60ccc9cf109d36829792c8de2a0afdceaab97cb
Brent Millare <brent.millare@gmail.com>
2011-06-27 11:04:59 -0400

    update install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-echo "Assumes java, git are installed"
+echo "Assumes java, git, unzip, and wget are installed"
 echo "Installing dj"
 git clone git://github.com/bmillare/dj.git
 cd dj
-mkdir -p usr/src usr/bin etc sbin opt tmp/dj var lib
+mkdir -p usr/src usr/bin etc sbin opt tmp var lib
 echo "Installing clojure"
 cd opt
-wget --no-check-certificate 'http://github.com/downloads/clojure/clojure/clojure-1.2.0.zip'
-unzip clojure-1.2.0.zip
+wget --no-check-certificate 'http://repo1.maven.org/maven2/org/clojure/clojure/1.3.0-beta1/clojure-1.3.0-beta1.zip'
+unzip clojure-1.3.0-beta1.zip
+rm clojure-1.3.0-beta1.zip
 cd ../lib
-ln -s ../opt/clojure-1.2.0/clojure.jar clojure.jar
+ln -s ../opt/clojure-1.3.0-beta1/clojure-1.3.0-beta1.jar clojure.jar
 
 echo "==================== IMPORTANT ===================="
 echo "Symlink dj/bin/run-dj-tool.sh to your path"
