667007fd9339ab07ad99ba45f164f01eff8f94a8
Brent Millare <brent.millare@gmail.com>
2011-06-29 08:47:11 -0400

    clojure folder bug

--- bin/install.sh
+++ bin/install.sh
@@ -3,7 +3,7 @@ echo "Assumes java, git, unzip, and wget are installed"
 echo "Installing dj"
 git clone git://github.com/bmillare/dj.git
 cd dj
-mkdir -p usr/src usr/bin etc sbin opt tmp/dj tmp/repository var lib
+mkdir -p usr/src/clojure usr/bin etc sbin opt tmp/dj tmp/repository var lib
 echo "Installing clojure"
 cd opt
 wget --no-check-certificate 'http://repo1.maven.org/maven2/org/clojure/clojure/1.3.0-beta1/clojure-1.3.0-beta1.zip'
