a9f651b59f02025f8a49a35b57a9864bbc79f7f5
Brent Millare <brent.millare@gmail.com>
2011-10-13 00:13:31 -0400

    fixed run and test regression bugs

--- src/dj/cli/run.clj
+++ src/dj/cli/run.clj
@@ -9,24 +9,18 @@
 (defn main
   "USAGE: dj run <filename> [project-name]"
   [filename & args]
-  (if-let [project-name (first args)]
-    (let [args (for [a (next args)]
-		 (if (= a "false")
-		   false
-		   (keyword a)))
-	  default-options {:verbose true
-			   :offline true}
-	  options (if (empty? args)
-		    default-options
-		    (apply assoc default-options args))
-	  [src-paths
-	   jar-paths
-	   native-paths] (dj.deps/obtain-dependencies!
-			  [(dj.deps.project.project-dependency. project-name)]
-			  options)]
-      (dj.classloader/with-new-classloader
-	src-paths
-	jar-paths
-	native-paths
-	`(load-file ~filename)))
+  (let [project-name (first args)
+	args (map read-string (next args))
+	default-options {:verbose true
+			 :offline true}
+	options (if (empty? args)
+		  default-options
+		  (apply assoc default-options args))
+	cl (clojure.lang.RT/baseLoader)]
+    (.setContextClassLoader (Thread/currentThread) cl)
+    (dj.classloader/add-dependencies! cl
+				      (if project-name
+					[project-name]
+					nil)
+				      options)
     (load-file filename)))
--- src/dj/cli/test.clj
+++ src/dj/cli/test.clj
@@ -1,37 +1,30 @@
 (ns dj.cli.test
   "run clojure tests from command line"
-  (:use [dj.toolkit :only [new-file]])
   (:require [dj.classloader]
 	    [dj.cli]
 	    [dj.deps.project]
 	    [dj.deps]
 	    [dj.core]
-	    [clojure.test]))
+	    [clojure.test]
+	    [dj.toolkit :as tk]))
 
 (defn main
   "USAGE: dj test <project-name> <namespaces>..."
   [project-name & namespaces]
   (if (and project-name namespaces)
-    (let [requires (map (fn [n]
-			  `(require '~(symbol n)))
-			namespaces)
-	  run-tests (map (fn [n]
-			  `(clojure.test/run-tests '~(symbol n)))
-			namespaces)
-	  
-	  default-options {:verbose true
+    (let [default-options {:verbose true
 			   :offline true}
 	  options default-options
-	  [src-paths jar-paths native-paths] (dj.deps/obtain-dependencies!
-					      [(dj.deps.project.project-dependency. project-name)]
-					      options)
-	  src-paths (conj src-paths (dj.io/string (new-file dj.core/system-root "usr/src" project-name "test")))]
-      (dj.classloader/with-new-classloader
-	src-paths
-	jar-paths
-	native-paths
-	`(do (require 'clojure.test)
-	     ~@requires
-	     ~@run-tests))
+	  cl (clojure.lang.RT/baseLoader)]
+      (.setContextClassLoader (Thread/currentThread) cl)
+      (dj.classloader/add-to-classpath! cl (.getCanonicalPath (tk/new-file dj.core/system-root "usr/src" project-name "test")))
+      (dj.classloader/add-dependencies! cl
+					[project-name]
+					options)
+      (require 'clojure.test)
+      (doseq [n namespaces]
+	(require [(symbol n)]))
+      (doseq [n namespaces]
+	(clojure.test/run-tests (symbol n)))
       (shutdown-agents))
     (println "USAGE: dj test <project-name> <namespaces>...")))
