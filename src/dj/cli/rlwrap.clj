ef311f3058accb4d9dfc09e9522b85ab80b4e81d
Brent Millare <brent.millare@gmail.com>
2011-10-13 00:21:41 -0400

    rlwrap updates

--- src/dj/cli/rlwrap.clj
+++ src/dj/cli/rlwrap.clj
@@ -8,9 +8,11 @@
    make-completions - generate completions file for core"
   [& [command]]
   (case command
-	"install" (println "todo, depends on os")
+	"install" (println "todo, depends on os, for ubuntu type $ sudo apt-get install rlwrap")
 	"make-completions"
 	(with-open [f (java.io.BufferedWriter. (java.io.FileWriter. (java.io.File. dj.core/system-root "etc/rlwrap-clj-completions")))]
 	  (.write f (apply str (interleave (keys (ns-publics (find-ns 'clojure.core))) (repeat "\n")))))
 
-	(println "command:" command "not found")))
+	(println "dj rlwrap [command]
+   install - install rlwrap (todo, depends on os)
+   make-completions - generate completions file for core")))
