865928e9b60968513c2b15fd1b81bae51ef56244
Brent Millare <brent.millare@gmail.com>
2012-08-14 21:51:45 -0400

    recur deps

--- src/dj/classloader.clj
+++ src/dj/classloader.clj
@@ -1,56 +1,13 @@
 (ns dj.classloader
   (:import [java.net URISyntaxException])
   (:require [dj]
+	    [cemerick.pomegranate :as pom]
 	    [dj.io :as io]
-	    [clojure.set :as set]))
+	    [clojure.set :as set])
+  (:refer-clojure :exclude (add-classpath)))
 
-(def boot-classpaths
-     (apply conj #{} (map #(io/file %)
-			  (.split (System/getProperty "java.class.path")
-				  (System/getProperty "path.separator")))))
-
-(defn url-to-file [^java.net.URL url]
-  (try
-    (java.io.File. ^java.net.URI (.toURI url))
-    (catch URISyntaxException e
-      (io/file (.getPath url)))))
-
-(defn get-classpaths [^java.net.URLClassLoader classloader]
-  (let [files (map url-to-file (.getURLs classloader))]
-    (if (empty? files)
-      boot-classpaths
-      (apply conj boot-classpaths files))))
-
-(defn get-current-classloader ^ClassLoader []
-  (.getContextClassLoader (Thread/currentThread)))
-
-(defn unchecked-add-to-classpath!
-  "adds file to classpath for classloader"
-  [classloader ^java.io.File f]
-  (let [clazz (class classloader)]
-    (if (= clazz
-	   sun.misc.Launcher$AppClassLoader)
-      (let [method (.getDeclaredMethod clazz "appendToClassPathForInstrumentation" (into-array Class [java.lang.String]))] 
-	(.setAccessible method true)
-	(.invoke method classloader (into-array Object [(.getPath f)])))
-      (.addURL ^java.net.URLClassLoader classloader
-	       ^java.net.URL (.toURL ^java.net.URI (.toURI f)))))
-  f)
-
-(defn add-to-classpath!
-  "given file, a jar or a directory, adds it to classpath for classloader
-
-   ASSUMES a jars with the same path are identical"
-  ([classloader path]
-     (let [f (if (= (first path)
-		    \/)
-	       (io/file path)
-	       (io/file dj/system-root path))]
-       (when-not ((get-classpaths classloader) f)
-	 (unchecked-add-to-classpath! classloader f))))
-  ([path]
-     (add-to-classpath! (.getParent (get-current-classloader))
-			path)))
+(dj/import-fn #'pom/add-classpath)
+(dj/import-fn #'pom/get-classpath)
 
 (defn reload-class-file
   "Reload a .class file during runtime, this allows you to recompile
@@ -75,12 +32,12 @@
     (System/setProperty "java.library.path" (apply str (interpose ";" native-paths)))))
 
 (defn resource-as-str [str-path]
-  (let [is (.getResourceAsStream (get-current-classloader) str-path)]
+  (let [is (.getResourceAsStream (first (pom/classloader-hierarchy)) str-path)]
     (apply str (map char (take-while #(not= % -1) (repeatedly #(.read is)))))))
 
 (defn find-resource
   (^java.io.File [relative-path]
-     (find-resource relative-path (.getParent (get-current-classloader))))
+     (find-resource relative-path (second (pom/classloader-hierarchy))))
   (^java.io.File [relative-path ^ClassLoader classloader]
      (io/file
       (.getPath
--- src/dj/dependencies.clj
+++ src/dj/dependencies.clj
@@ -1,22 +1,77 @@
 (ns dj.dependencies
-  (:require [cemerick.pomegranate :as pom]
+  (:require [leiningen.core.project :as project]
 	    [dj]
-	    [dj.io]))
+	    [dj.io]
+	    [dj.git]))
 
-(defn extract-project-data [path]
-  (let [[_ project-name version & rest] (read-string (slurp path))]
-    (assoc (apply hash-map rest)
-      :name (str project-name)
-      :version version)))
+(defn project-read-str
+  "like leiningen.core.project/read but from a string instead of a file"
+  ([proj-str profiles]
+     (binding [*ns* (the-ns 'leiningen.core.project)]
+       (eval (read-string proj-str)))
+     (let [project (resolve 'leiningen.core.project/project)]
+       (when-not project
+	 (throw (Exception. "project.clj must define project map.")))
+       ;; return it to original state
+       (ns-unmap 'leiningen.core.project 'project)
+       (-> (reduce project/apply-middleware @project (:middleware @project))
+	   (project/merge-profiles profiles))))
+  ([proj-str] (project-read-str proj-str [:default])))
 
-(defn add-dependencies! [dependency-objects]
-  (pom/add-dependencies :coordinates dependency-objects))
+(defmulti resolve-dj-dependency :dependency-type)
 
 (defn resolve-project [relative-path]
-  (let [project-dir (dj.io/file dj/system-root "usr/src" relative-path)]
-    (-> (dj.io/file project-dir "project.clj")
-	extract-project-data
-	:dependencies
-	add-dependencies!)))
+  (let [project-dir (dj.io/file dj/system-root "usr/src" relative-path)
+	project-data (-> (dj.io/file project-dir "project.clj")
+			 slurp
+			 project-read-str
+			 (assoc :root (dj.io/get-path project-dir)
+				:eval-in :leiningen))]
+    (if-let [dj-dependencies (:dj/dependencies project-data)]
+      (doall (map (comp resolve-dj-dependency parse-dj-project-dependency) dj-dependencies))
+      (project/init-project project-data))))
+;; we want to be able to resolve a project, then we can learn to resolve a git repo
 
-;; need to add source dependencies, like that use git, and somehow make this recursive
+;; keywords might clash, one potential fix is to used namespaced
+;; keywords, i can guarantee that all the keywords are not fully
+;; qualified
+
+;; what i need
+;; -a string parser that will return a data object with details of the dependency
+
+(defn parse-dj-project-dependency [entry]
+  (if (= java.lang.String (type entry))
+    (let [components (.split #"/" entry)]
+      (if (re-find #"http://|git://|https://|ssh://" entry)
+	(let [[_ n] (re-find #"((?:\w|-|_)+)\.git" (last components))]
+	  {:dependency-type :git
+	   :name n
+	   :git-path entry})
+	(if (= "clojure" (first components))
+	  {:dependency-type :git
+	   :name entry
+	   :git-path (str "git://github.com/" entry ".git")}
+	  {:dependency-type :source
+	   :name (last components)
+	   :relative-path entry})))
+    entry))
+
+;; first I need the local path, this is from the name?  i'd like a way
+;; to do namespace as in nested folders, but these git folders are
+;; more like remote trackers, as long as you acknowledge that
+;; limitation than its fine, you can do more custom version controlled
+;; content with source types
+(defmethod resolve-dj-dependency :git [entry-obj]
+	   (let [f (dj.io/file dj/system-root "usr/src" (:name entry-obj))]
+	     (when-not (dj.io/exists? f)
+	       (dj.git/clone (:git-path entry-obj)))
+	     (resolve-project (dj.io/get-path f))))
+
+;; i don't want to fully commit to "usr/src"
+;; i'd like a verbose mode, where a map is passed
+(defmethod resolve-dj-dependency :source [entry-obj]
+	   (let [relative-path (:relative-path entry-obj)
+		 f (if relative-path
+		     (dj.io/file dj/system-root "usr/src" relative-path)
+		     (dj.io/file dj/system-root (:root-path entry-obj)))]
+	     (resolve-project (dj.io/get-path f))))
