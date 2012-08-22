699dcf6f55b9cdf1018a588d0fffc1381248508e
Brent Millare <brent.millare@gmail.com>
2012-08-22 18:50:59 -0400

    to allow nrepl support for clojurescript

--- project.clj
+++ project.clj
@@ -2,4 +2,7 @@
   :description "A library of utilities"
   :dependencies [[org.clojure/clojure "1.4.0"]
 		 [org.eclipse.jgit/org.eclipse.jgit "2.0.0.201206130900-r"]
-		 [leiningen-core "2.0.0-SNAPSHOT"]])
+		 [leiningen-core "2.0.0-SNAPSHOT"]
+		 [com.cemerick/piggieback "0.0.2"]]
+  :injections [(require 'cemerick.piggieback)]
+  :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]})
