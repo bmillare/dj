dcbdc93145e24588037c791486f087723d402bf1
Brent Millare <brent.millare@gmail.com>
2012-09-10 15:15:31 -0400

    cljs-repl documentation

--- src/dj/cljs/piggieback.clj
+++ src/dj/cljs/piggieback.clj
@@ -3,7 +3,14 @@
 	    [cljs.repl]
 	    [cljs.repl.browser]))
 
-(defn cljs-repl [{:keys [port working-dir]}]
+(defn cljs-repl
+  "port: for repl/server
+   working-dir: path/file to generated js
+   To connect to server, run cljs in browser (clojure.browser.repl/connect \"http://localhost:<port>/repl\")
+   Make sure advanced optimizations is not activated. Simple works though.
+
+   Use load-file or load-namespace to do dynamic development"
+  [{:keys [port working-dir]}]
   (cemerick.piggieback/cljs-repl
    :repl-env (doto (cljs.repl.browser/repl-env :port port :working-dir working-dir)
 	       cljs.repl/-setup)))
