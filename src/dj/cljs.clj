4dde724b1a2cbafc95c2bf43b76e99ca1ca402ec
Brent Millare <brent.millare@gmail.com>
2012-09-10 15:23:18 -0400

    reorganize cljs directory

--- src/dj/cljs.clj
+++ src/dj/cljs.clj
@@ -1,10 +1,12 @@
 (ns dj.cljs
   (:require [dj]
-	    [dj.cljs.piggieback]
 	    [dj.git]
 	    [dj.io]
 	    [dj.classloader]
-	    [clojure.java.shell :as sh]))
+	    [clojure.java.shell :as sh]
+	    [cemerick.piggieback]
+	    [cljs.repl]
+	    [cljs.repl.browser]))
 
 (defn add-cljs-to-classpath!
   ([cljs-dir]
@@ -27,3 +29,15 @@
 		    cljs-dir)
       (sh/sh "script/bootstrap"
 	     :dir (dj.io/file cljs-dir "clojurescript")))))
+
+(defn cljs-repl
+  "port: for repl/server
+   working-dir: path/file to generated js
+   To connect to server, run cljs in browser (clojure.browser.repl/connect \"http://localhost:<port>/repl\")
+   Make sure advanced optimizations is not activated. Simple works though.
+
+   Use load-file or load-namespace to do dynamic development"
+  [{:keys [port working-dir]}]
+  (cemerick.piggieback/cljs-repl
+   :repl-env (doto (cljs.repl.browser/repl-env :port port :working-dir working-dir)
+	       cljs.repl/-setup)))
