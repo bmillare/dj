f1ed2493cb1ace5e0570364d5390859cfc306d70
Brent Millare <brent.millare@gmail.com>
2012-09-18 23:24:36 -0400

    cljs eval and git

--- src/dj/cljs.clj
+++ src/dj/cljs.clj
@@ -5,6 +5,7 @@
 	    [dj.classloader]
 	    [clojure.java.shell :as sh]
 	    [cemerick.piggieback]
+	    [cljs.analyzer]
 	    [cljs.repl]
 	    [cljs.repl.browser]))
 
@@ -30,6 +31,8 @@
       (sh/sh "script/bootstrap"
 	     :dir (dj.io/file cljs-dir "clojurescript")))))
 
+(def cljs-repl-envs (atom []))
+
 (defn cljs-repl
   "port: for repl/server
    working-dir: path/file to generated js
@@ -38,6 +41,23 @@
 
    Use load-file or load-namespace to do dynamic development"
   [{:keys [port working-dir]}]
-  (cemerick.piggieback/cljs-repl
-   :repl-env (doto (cljs.repl.browser/repl-env :port port :working-dir working-dir)
-	       cljs.repl/-setup)))
+  (let [repl-env (doto (cljs.repl.browser/repl-env :port port :working-dir working-dir)
+		   cljs.repl/-setup)]
+    (swap! cljs-repl-envs conj repl-env)
+    (cemerick.piggieback/cljs-repl
+     :repl-env repl-env)))
+
+(defn cljs-eval
+  ([repl-env env filename form]
+     (cljs.repl/evaluate-form repl-env env filename form))
+  ([repl-env form]
+     (cljs.repl/evaluate-form repl-env
+			      {:context :statement
+			       :locals {}
+			       :ns (cljs.analyzer/get-namespace 'cljs.user)}
+			      "<dj.cljs/cljs-eval>" form))
+  ([form]
+     (let [repl-env (first @cljs-repl-envs)]
+       (if repl-env
+	 (cljs-eval repl-env form)
+	 (throw (Exception. "No existing repl-env found in dj.cljs/cljs-repl-envs"))))))
--- src/dj/git.clj
+++ src/dj/git.clj
@@ -12,5 +12,22 @@
   ([^java.lang.String uri]
      (clone uri (dj.io/file dj/system-root "usr/src"))))
 
-#_ (defn pull []
-     (org.eclipse.jgit.api.PullCommand. ))
+(defn pull [file]
+  (-> (.pull (org.eclipse.jgit.api.Git.
+	      (org.eclipse.jgit.storage.file.FileRepository.
+	       file)))
+    (.call)))
+
+(defn push [file]
+  (-> (.push (org.eclipse.jgit.api.Git.
+	      (org.eclipse.jgit.storage.file.FileRepository.
+	       file)))
+      (.call)))
+
+(defn commit [file options]
+  (let [{:keys [all amend author message]} options
+	c (.commit (org.eclipse.jgit.api.Git/open file))]
+    (when all
+      (.setAll c true))
+    (.setMessage c (or message "default"))
+    (.call c)))
