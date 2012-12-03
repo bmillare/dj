245a9e743b5a6103e0953669108961d4a2cf63d9
Brent Millare <brent.millare@gmail.com>
2012-12-03 17:45:52 -0500

    source stuff

--- src/dj/source.clj
+++ src/dj/source.clj
@@ -2,31 +2,50 @@
   (:refer-clojure :exclude [re-find])
   (:require [dj.io]))
 
-(defn find [dir f]
+(defn find
+  "searchers directory for files that when passed to f, return true"
+  [dir f]
   (reduce (fn [ret file]
-            (if (f file)
-              (if (.isDirectory file)
-                (concat (find file f) ret)
-                (cons file ret))
-              ret))
+            (if (.isDirectory file)
+              (concat (find file f) ret)
+              (if (f file)
+                (cons file ret)
+                ret)))
           ()
           (dj.io/ls dir)))
 
-(defn re-find [dir re]
+(defn re-find
+  "searches directory for files that have name re"
+  [dir re]
   (find dir
         (fn [file]
           (clojure.core/re-find re (dj.io/get-name file)))))
 
-(defn search [dir re-filename re-text]
+(defn search
+  "searches directory for files that have re-text"
+  [dir re-filename re-text]
   (for [f (re-find dir re-filename)
         :when (clojure.core/re-find re-text (dj.io/eat f))]
     f))
 
-(defn defs [f]
-  (let [re #"\(def\w*\s+(\S+)"]
+(defn defs
+  "given a file, searches for all 'def' like forms and returns first
+  arg"
+  [f]
+  (let [re #"\(def\S*\s+(\S+)"]
     (map (fn [m]
            (second
             (re-matches re
                         m)))
          (dj/re-find-all re
                          (dj.io/eat f)))))
+
+(defn map-files
+  "creates a hashmap of filenames to output of fn given file"
+  [f files]
+  (reduce (fn [m file]
+            (assoc m
+              (dj.io/get-path file)
+              (f file)))
+          {}
+          files))
