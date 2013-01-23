8bb7334be8acd9d41086bfdf42c9975fb0d4a42f
Brent Millare <brent.millare@gmail.com>
2013-01-23 10:38:19 -0500

    upgrade

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject dj "2.0.0"
   :description "A library of utilities"
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.0-RC2"]
 		 [org.eclipse.jgit/org.eclipse.jgit "2.0.0.201206130900-r"]
-		 [leiningen-core "2.0.0-preview10"]
+		 [leiningen-core "2.0.0"]
 		 [org.apache.directory.studio/org.apache.commons.io "2.1"]
                  [org.clojure/tools.namespace "0.2.0"]]
   :injections [(require '[dj.repl]
