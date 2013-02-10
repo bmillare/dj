a44b7e8492250dadc4966d017bedb9dfbc72c0c4
Brent Millare <brent.millare@gmail.com>
2013-02-09 23:41:29 -0500

    datomic update and profile.clj

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
   :injections [(require '[dj.repl]
 			'[dj.dependencies]
 			'[clojure.tools.namespace.repl])]
-  :profiles {:database {:dependencies [[com.datomic/datomic-free "0.8.3731"]]}
+  :profiles {:database {:dependencies [[com.datomic/datomic-free "0.8.3789"]]}
              :cljs {:dependencies [[org.clojure/google-closure-library "0.0-2029"]
                                    [org.clojure/google-closure-library-third-party "0.0-2029"]]}}
   :jvm-opts ["-XX:MaxPermSize=256M"])
