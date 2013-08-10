f189a961941b1306d255d0a1938b7503aadcdb5c
Brent Millare <brent.millare@gmail.com>
2013-08-10 19:08:55 -0400

    project update cljs

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject dj "2.0.1"
   :description "A library of utilities"
   :dependencies [[org.clojure/clojure "1.5.1"]
-		 [org.eclipse.jgit/org.eclipse.jgit "2.0.0.201206130900-r"]
+                 [org.eclipse.jgit/org.eclipse.jgit "2.0.0.201206130900-r"]
 		 [leiningen-core "2.1.3"]
 		 [org.apache.directory.studio/org.apache.commons.io "2.1"]
                  [org.clojure/tools.namespace "0.2.0"]]
@@ -10,5 +10,7 @@
 			'[clojure.tools.namespace.repl])]
   :profiles {:database {:dependencies [[com.datomic/datomic-free "0.8.3789"]]}
              :cljs {:dependencies [[org.clojure/google-closure-library "0.0-2029"]
-                                   [org.clojure/google-closure-library-third-party "0.0-2029"]]}}
+                                   [org.clojure/google-closure-library-third-party "0.0-2029"]
+                                   [org.clojure/tools.reader "0.7.5"]
+                                   [org.clojure/data.json "0.2.2"]]}}
   :jvm-opts ["-XX:MaxPermSize=256M"])
